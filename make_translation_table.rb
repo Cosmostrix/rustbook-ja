6b7d593b0879ac3392bb47c2754f9300b56df411
Sorafukurou <cosmos@lunakit.org>
2015-12-07 19:54:04 +0900

    訳語表生成

---
+++ make_translation_table.rb
@@ -0,0 +1,12 @@
+require 'yaml'
+require 'pp'
+
+table = YAML.load(File.read('Project明治回帰.yaml')).sort_by {|x| x['en'].downcase }
+puts File.read('translation-table-template.md')
+puts '| 英 | 和 | 参考訳 | ＊ |'
+puts '|----|----|--------|----|'
+table.each do |x|
+  next unless x['en'] && x['ja']
+  #next if x['common']
+  puts ['', x['en'], x['ja'], x['common'], x['yamato']].join('|')
+end
